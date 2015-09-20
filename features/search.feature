8ac0ad2e565b0888a50a3c75386dbf3e74e2b599
Jovan Ivanovic <jovan.ivanovic3@gmail.com>
2015-09-20 12:04:11 +0200

    Add cucumber tests

---
+++ features/search.feature
@@ -0,0 +1,14 @@
+Feature: Search
+  In order to find a link
+  As a user
+  I want to input links
+  And run the search
+  And watch the results
+
+  Background:
+    Given that I'm on the home page
+
+  Scenario: Start search
+    Given that I've set two correct links
+    When I run search
+    Then I should get a response confirming the start of search
---
+++ features/step_definitions/start_search.rb
@@ -0,0 +1,15 @@
+Given(/^that I'm on the home page$/) do
+  visit("/")
+end
+
+Given(/^that I've set two correct links$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I run search$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should get a response confirming the start of search$/) do
+    pending # express the regexp above with the code you wish you had
+end
